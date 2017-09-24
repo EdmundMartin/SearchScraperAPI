268c357596187f6ef798d81ebf539f3eb46edee3
EdmundMartin <edmartin101@googlemail.com>
2017-09-24 10:45:18 +0100

    Updated setup.py

--- setup.py
+++ setup.py
@@ -3,11 +3,12 @@ from distutils.core import setup
 setup(
     name='searchscrapeserver',
     packages=['searchscrapeserver'],
+    package_dir={'searchscrapeserver': 'searchscrapeserver'},
     version='0.55',
     description='Server that allows you to scrape search engines',
     author='Edmund Martin',
     author_email='edmartin101@googlemail.com',
     url='https://github.com/EdmundMartin/SearchScraperAPI',
-    download_url='https://github.com/EdmundMartin/SearchScraperAPI/archive/0.01.tar.gz'
-    
+    download_url='https://github.com/EdmundMartin/SearchScraperAPI/archive/0.02.tar.gz',
+
 )
