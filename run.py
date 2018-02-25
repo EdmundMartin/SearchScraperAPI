3278a021d856862a35e2e44615b47e01c45fb804
EdmundMartin <edmartin101@googlemail.com>
2018-02-25 17:40:03 +0000

    Set back properly for docker

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 from searchscrapeserver import SearchScraper
 
 if __name__ == '__main__':
-    server = SearchScraper('127.0.0.1', 8080)
+    server = SearchScraper('0.0.0.0', 5000)
     server.run_server()
